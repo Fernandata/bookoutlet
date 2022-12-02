c7f72f7756cc8114743d59ccf8569fcc46e0ea65
Luis Fernando Salas Gave <lsalagav@emeal.nttdata.com>
2022-12-02 01:35:27 -0500

    Create login views

---
+++ Book/CreateAccountViewController.swift
@@ -0,0 +1,32 @@
+//
+//  CreateAccountViewController.swift
+//  Book
+//
+//  Created by Luis Fernando Salas Gave on 2/12/22.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func signUpClicked(_ sender: UIButton) {
+        print("signup")
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Book/LoginViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LoginViewController.swift
+//  Book
+//
+//  Created by Luis Fernando Salas Gave on 2/12/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func loginClicked(_ sender: UIButton) {
+        print("works")
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
